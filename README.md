64e6e6b568d47db55926571e0da838f397e09f5c
momo <zz3110420437@163.com>
2023-03-28 14:46:41 +0800

    异常页面

--- README.md
+++ README.md
@@ -1,3 +1,17 @@
 # reactadmin
 
-react 的一个后台管理系统
+react 的一个后台管理系统框架
+
+# 快速开始
+
+```
+cd reactadmin
+
+
+pnpm i
+
+pnpm dev
+
+pnpm build
+
+```
--- src/pages/NotFound/index.jsx
+++ src/pages/NotFound/index.jsx
@@ -1,5 +1,16 @@
-import React from "react"
+import { Link } from "react-router-dom"
+import { Result, Button } from "antd"
 
-export default function index() {
-  return <div>404</div>
-}
+const NotFound = () => (
+  <Result
+    status="404"
+    title="404"
+    subTitle="抱歉，你访问的页面不存在！"
+    extra={
+      <Link to="/">
+        <Button type="primary">返回首页</Button>
+      </Link>
+    }
+  />
+)
+export default NotFound
